3840388f4f46eff91969781b60e9169c62e09422
Enoobong Ibanga <enoobong@bundle.africa>
2021-06-15 15:17:52 +0100

    initial commit

---
+++ src/main/java/com/example/getmatitest/GetMatiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.getmatitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetMatiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetMatiTestApplication.class, args);
+    }
+
+}
